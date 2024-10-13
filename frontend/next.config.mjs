13be53d869f56715c06b7f6497f901c7a50ddc73
walnuts1018 <r.juglans.1018@gmail.com>
2024-10-14 00:23:17 +0900

    add
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  images: {
+    formats: ["image/webp"],
+  },
+  poweredByHeader: false,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
