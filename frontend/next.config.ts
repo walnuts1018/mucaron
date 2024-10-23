9fdf7b1442b2a3437b330e2e0a9aed5a79182344
walnuts1018 <r.juglans.1018@gmail.com>
2024-10-24 00:30:50 +0900

    standalone
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    formats: ["image/webp"],
+  },
 };
 
 export default nextConfig;
