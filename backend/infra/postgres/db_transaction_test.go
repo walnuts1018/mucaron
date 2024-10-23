543b5318ec5059367ae81eb3a4e9be7715d3443e
walnuts1018 <r.juglans.1018@gmail.com>
2024-10-24 02:39:22 +0900

    testobject private
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- backend/infra/postgres/db_transaction_test.go
+++ backend/infra/postgres/db_transaction_test.go
@@ -13,13 +13,13 @@ import (
 var _ = Describe("Transaction", func() {
 	ctx := context.Background()
 
-	to1 := TestObject{
+	to1 := testObject{
 		UUIDModel: gormmodel.UUIDModel{
 			ID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
 		},
 		Name: "test1",
 	}
-	to2 := TestObject{
+	to2 := testObject{
 		UUIDModel: gormmodel.UUIDModel{
 			ID: uuid.MustParse("00000000-0000-0000-0000-000000000002"),
 		},
@@ -28,41 +28,41 @@ var _ = Describe("Transaction", func() {
 
 	It("Test Transaction", func() {
 		By("auto migrate")
-		err := p.DB(ctx).AutoMigrate(&TestObject{})
+		err := p.DB(ctx).AutoMigrate(&testObject{})
 		Expect(err).ShouldNot(HaveOccurred())
 
 		By("should commit")
 		err = p.Transaction(ctx, func(ctx context.Context) error {
 			By("create test object 1")
-			err := p.CreateTestObject(ctx, to1)
+			err := p.createTestObject(ctx, to1)
 			Expect(err).ShouldNot(HaveOccurred())
 
 			By("get test object 1")
-			to1copy, err := p.GetTestObjectByID(ctx, to1.ID)
+			to1copy, err := p.getTestObjectByID(ctx, to1.ID)
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(to1copy.Name).To(Equal(to1.Name))
 
 			By("update test object 1")
 			to1copy.Name = "test1copy"
-			err = p.UpdateTestObject(ctx, to1copy)
+			err = p.updateTestObject(ctx, to1copy)
 			Expect(err).ShouldNot(HaveOccurred())
 
 			By("get test object 1 copy")
-			to1copy, err = p.GetTestObjectByID(ctx, to1.ID)
+			to1copy, err = p.getTestObjectByID(ctx, to1.ID)
 			Expect(err).ShouldNot(HaveOccurred())
 			return nil
 		})
 		Expect(err).ShouldNot(HaveOccurred())
 
 		By("get test object 1 copy")
-		to1copy, err := p.GetTestObjectByID(ctx, to1.ID)
+		to1copy, err := p.getTestObjectByID(ctx, to1.ID)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(to1copy.Name).To(Equal("test1copy"))
 
 		By("should rollback")
 		err = p.Transaction(ctx, func(ctx context.Context) error {
 			By("create test object 2")
-			err := p.CreateTestObject(ctx, to2)
+			err := p.createTestObject(ctx, to2)
 			Expect(err).ShouldNot(HaveOccurred())
 
 			By("return error")
@@ -71,8 +71,8 @@ var _ = Describe("Transaction", func() {
 		Expect(err).Should(HaveOccurred())
 
 		By("test object 2 should not exist")
-		to2copy, err := p.GetTestObjectByID(ctx, to2.ID)
+		to2copy, err := p.getTestObjectByID(ctx, to2.ID)
 		Expect(err).Should(HaveOccurred())
-		Expect(to2copy).To(Equal(TestObject{}))
+		Expect(to2copy).To(Equal(testObject{}))
 	})
 })
